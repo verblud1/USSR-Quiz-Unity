52e5dbeed3d262b6036ae4d9937aaea311700a21
verblud1 <bogasda92@gmail.com>
2025-11-08 08:04:32 +0300

    test change

--- Assets/Scripts/StartMenuAnimationButton.cs
+++ Assets/Scripts/StartMenuAnimationButton.cs
@@ -4,7 +4,7 @@ using DG.Tweening;
 public class StartMenuAnimationButton : MonoBehaviour
 {
     private Transform ButtonTransform;
-    public float hoverScale = 1.1f
+    public float hoverScale = 1.1f;
     void Start()
     {
         ButtonTransform.DOScale(new Vector3(2f,2f,2f),1f);
